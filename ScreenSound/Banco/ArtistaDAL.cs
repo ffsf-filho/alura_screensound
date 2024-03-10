8986dfe552978c544337fa717206a58048d9fed2
ffsf-filho <ffsf.filho@gmail.com>
2024-03-10 18:29:21 -0300

    Inclui métodos Atualizar e Deletar na classe ArtistaDAL

--- ScreenSound/Banco/ArtistaDAL.cs
+++ ScreenSound/Banco/ArtistaDAL.cs
@@ -63,4 +63,49 @@ internal class ArtistaDAL
             Console.WriteLine(ex.Message);
         }
     }
+
+    public void Atualizar(Artista artista)
+    {
+        try
+        {
+            string sql = "UPDATE Artistas SET Nome = @nome, Bio = @bio, FotoPerfil = @perfilPadrao WHERE Id = @id";
+
+            using SqlConnection connection = new Connection().ObterConexao();
+            connection.Open();
+
+            using SqlCommand command = new(sql, connection);
+            command.Parameters.AddWithValue("@nome", artista.Nome);
+            command.Parameters.AddWithValue("@bio", artista.Bio);
+            command.Parameters.AddWithValue("@perfilPadrao", artista.FotoPerfil);
+            command.Parameters.AddWithValue("@id", artista.Id);
+
+            int retorno = command.ExecuteNonQuery();
+            Console.WriteLine($"Linhas afetadas: {retorno}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    public void Deletar(Artista artista)
+    {
+        try
+        {
+            string sql = "DELETE FROM Artistas WHERE Id = @id";
+
+            using SqlConnection connection = new Connection().ObterConexao();
+            connection.Open();
+
+            using SqlCommand command = new(sql, connection);
+            command.Parameters.AddWithValue("@id", artista.Id);
+
+            int retorno = command.ExecuteNonQuery();
+            Console.WriteLine($"Linhas afetadas: {retorno}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
 }
