4c8165c3358c9e86040b9cd23b8d9c35c594ba92
ffsf-filho <ffsf.filho@gmail.com>
2024-03-10 15:51:49 -0300

    Conectando ao banco de dados

---
+++ ScreenSound/Banco/Connection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Banco;
+
+internal class Connection
+{
+    private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    //private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+    public SqlConnection ObterConexao()
+    {
+        return new SqlConnection(connectionString);
+    }
+}
--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,5 +1,20 @@
 ﻿using ScreenSound.Menus;
 using ScreenSound.Modelos;
+using ScreenSound.Banco;
+using Microsoft.Data.SqlClient;
+
+try
+{
+    using SqlConnection connection = new Connection().ObterConexao();
+    connection.Open();
+    Console.WriteLine(connection.State);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+return;
 
 Artista ira = new Artista("Ira!", "Banda Ira!");
 Artista beatles = new("The Beatles", "Banda The Beatles");
