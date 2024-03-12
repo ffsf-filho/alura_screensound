32fa47181a5b01087dbfcdfcf33fc5a3b497702f
ffsf-filho <ffsf.filho@gmail.com>
2024-03-12 00:01:31 -0300

    Cria a tabela Musicas no bando de dados, refatora a classe de contexto e implementa a classe MusicaDAL

--- ScreenSound/Banco/ScreenSoundContext.cs
+++ ScreenSound/Banco/ScreenSoundContext.cs
@@ -9,6 +9,7 @@ internal class ScreenSoundContext : DbContext
     //private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
     
     public DbSet<Artista> Artistas { get; set; }
+    public DbSet<Musica> Musicas { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
