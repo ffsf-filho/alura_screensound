a2b562dff77132187443411586e43a98a378727c
ffsf-filho <ffsf.filho@gmail.com>
2024-03-14 23:07:19 -0300

    Cria Migrattio para adicionando Informações na nova coluna ArtistaId em Musicas

---
+++ ScreenSound/Migrations/20240315015759_PopulaColunaArtistaId.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations;
+
+/// <inheritdoc />
+public partial class PopulaColunaArtistaId : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql("UPDATE Musicas SET ArtistaId = 1 WHERE Id IN(1,2,3,4)");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql("UPDATE Musicas SET ArtistaId = null WHERE Id IN(1,2,3,4)");
+    }
+}
