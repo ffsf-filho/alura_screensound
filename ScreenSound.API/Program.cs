505ecc638363ed11c97e9a588db4baf795ce3010
franciscof <franciscof@cetsp.com.br>
2024-03-16 13:41:58 -0300

    Retorna Artista no projeto de API

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -1,6 +1,19 @@
+using ScreenSound.Banco;
+using ScreenSound.Modelos;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(
+    options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () =>
+{
+    DAL<Artista> dal = new(new ScreenSoundContext());
+    IEnumerable<Artista> lista =  dal.Listar();
+
+    return lista;
+});
 
 app.Run();
