cffafe3e6afaa0fb0207eda5bbd66f848fd284fb
ffsf-filho <ffsf.filho@gmail.com>
2024-03-26 00:08:59 -0300

    Inclui as classes ArtistaResponse e MusicaResponse

---
+++ ScreenSound.API/Response/ArtistaResponse.cs
@@ -0,0 +1,16 @@
+﻿using ScreenSound.Modelos;
+
+namespace ScreenSound.API.Response;
+
+public record ArtistaResponse(int Id, string Nome, string Bio, string? FotoPerfil)
+{
+    private static ICollection<ArtistaResponse> EntityListToResponseList(IEnumerable<Artista> listaDeArtistas)
+    {
+        return listaDeArtistas.Select(a => EntityToResponse(a)).ToList();
+    }
+
+    private static ArtistaResponse EntityToResponse(Artista artista)
+    {
+        return new ArtistaResponse(artista.Id, artista.Nome, artista.Bio, artista.FotoPerfil);
+    }
+}
---
+++ ScreenSound.API/Response/MusicaResponse.cs
@@ -0,0 +1,16 @@
+﻿using ScreenSound.Modelos;
+
+namespace ScreenSound.API.Response;
+
+public record MusicaResponse(int Id, string Nome, int ArtistaId, string NomeArtista)
+{
+    private static ICollection<MusicaResponse> EntityListToResponseList(IEnumerable<Musica> musicaList)
+    {
+        return musicaList.Select(a => EntityToResponse(a)).ToList();
+    }
+
+    private static MusicaResponse EntityToResponse(Musica musica)
+    {
+        return new MusicaResponse(musica.Id, musica.Nome!, musica.Artista!.Id, musica.Artista.Nome);
+    }
+}
