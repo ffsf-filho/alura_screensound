4643f85bd97e9426535b12af6cb139d9c464b703
ffsf-filho <ffsf.filho@gmail.com>
2024-04-07 11:02:38 -0300

    Prepara para o consumo da API Artistas

---
+++ ScreenSound.Web/Requests/ArtistaRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace ScreenSound.Web.Requests;
+
+public record ArtistaRequest(string nome, string bio);
---
+++ ScreenSound.Web/Response/GeneroResponse.cs
@@ -0,0 +1,3 @@
+﻿namespace ScreenSound.Web.Response;
+
+public record GeneroResponse(int Id, string Nome, string Descricao);
