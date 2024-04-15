1664768349791e64534ebebc89ebdbc2e332b019
ffsf-filho <ffsf.filho@gmail.com>
2024-04-15 20:50:28 -0300

    Implementa CardMusica e refatora as classes GeneroResponse e GeneroRequest e cria class GeneroAPI

--- ScreenSound.API/Response/GeneroResponse.cs
+++ ScreenSound.API/Response/GeneroResponse.cs
@@ -1,3 +1,9 @@
 ﻿namespace ScreenSound.API.Response;
 
-public record GeneroResponse(int Id, string Nome, string Descricao);
+public record GeneroResponse(int Id, string Nome, string Descricao)
+{
+    public override string ToString()
+    {
+        return $"{this.Nome}";
+    }
+}
---
+++ ScreenSound.Web/Services/GeneroAPI.cs
@@ -0,0 +1,18 @@
+﻿using ScreenSound.Web.Response;
+using System.Net.Http.Json;
+
+namespace ScreenSound.Web.Services;
+
+public class GeneroAPI(IHttpClientFactory factory)
+{
+    private readonly HttpClient _httpClient = factory.CreateClient("API");
+
+    public async Task<List<GeneroResponse>?> GetGenerosAsync()
+    {
+        return await _httpClient.GetFromJsonAsync<List<GeneroResponse>>("generos");
+    }
+    public async Task<GeneroResponse?> GetGeneroPorNomeAsync(string nome)
+    {
+        return await _httpClient.GetFromJsonAsync<GeneroResponse>($"generos/{nome}");
+    }
+}
