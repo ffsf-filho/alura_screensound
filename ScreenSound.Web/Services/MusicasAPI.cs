ea802d78ca3172971d25e6287e0f9b28c94938d9
ffsf-filho <ffsf.filho@gmail.com>
2024-04-15 22:50:19 -0300

    Implementa cadastrar música

--- ScreenSound.Web/Services/MusicasAPI.cs
+++ ScreenSound.Web/Services/MusicasAPI.cs
@@ -1,4 +1,5 @@
-﻿using ScreenSound.Web.Response;
+﻿using ScreenSound.Web.Requests;
+using ScreenSound.Web.Response;
 using System.Net.Http.Json;
 
 namespace ScreenSound.Web.Services;
@@ -10,5 +11,10 @@ public class MusicasAPI(IHttpClientFactory factory)
 	public async Task<ICollection<MusicaResponse>?> GetMusicasAsync()
 	{
 		return await _httpClient.GetFromJsonAsync<ICollection<MusicaResponse>>("Musicas");
+	}
+
+	public async Task AddMusicaAsync(MusicaRequest musica)
+	{
+		await _httpClient.PostAsJsonAsync("Musicas", musica);
 	}
 }
